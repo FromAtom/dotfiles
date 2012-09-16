f05d96034ba8666fe44dcbabdf0eeb6234d648d1
Atom <fromatom@gmail.com>
2012-09-16 12:51:53 +0900

    install auto-complete

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -62,12 +62,10 @@
 
 
 
-
-
-
-
-
-
+(add-to-list 'load-path "~/.emacs.d/elisp/")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/elisp//ac-dict")
+(ac-config-default)
 
 
 
