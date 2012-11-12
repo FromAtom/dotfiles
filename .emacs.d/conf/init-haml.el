67c99ef2bdee19e49745ba42da83fc14dc5840d6
Atom <fromatom@gmail.com>
2012-11-12 22:44:36 +0900

    HamlとSCSS用にインデントハイライト機能を追加

--- .emacs.d/conf/init-haml.el
+++ .emacs.d/conf/init-haml.el
@@ -4,3 +4,13 @@
           (lambda ()
             (setq indent-tabs-mode nil)
             (define-key haml-mode-map "\C-m" 'newline-and-indent)))
+
+(require 'highlight-indentation)
+(setq highlight-indentation-offset 2)
+(set-face-background 'highlight-indentation-face "#808080")
+(set-face-background 'highlight-indentation-current-column-face "#606060")
+
+(add-hook 'coffee-mode-hook 'highlight-indentation-current-column-mode)
+(add-hook 'cperl-mode-hook  'highlight-indentation-current-column-mode)
+(add-hook 'scss-mode-hook   'highlight-indentation-current-column-mode)
+(add-hook 'haml-mode-hook   'highlight-indentation-current-column-mode)
