2ae373c8609710d6c4c2bb5e8201311017238d36
Atom <fromatom@gmail.com>
2015-11-20 11:19:25 +0900

    ruby でcoding: utf-8を消す

--- .emacs.d/inits/20-ruby-mode.el
+++ .emacs.d/inits/20-ruby-mode.el
@@ -1,3 +1,6 @@
 ;; RakefileとGemfileもruby-modeで開く
 (add-to-list 'auto-mode-alist '("[Rr]akefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+
+;; # coding: utf-8 は不要
+(setq ruby-insert-encoding-magic-comment nil)
