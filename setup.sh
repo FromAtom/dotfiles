bfd5463d056681fd59e9705b801d55173540a462
Atom <fromatom@gmail.com>
2018-01-24 13:11:02 +0900

    macOS向けのsetupスクリプトを追記

--- setup.sh
+++ setup.sh
@@ -9,5 +9,15 @@ cd $THIS_DIR
 DOT_FILES=( .zsh .zshrc .emacs.d .gitconfig .gitignore )
 for file in ${DOT_FILES[@]}
 do
-  ln -s ${THIS_DIR}/$file ${HOME}
+    ln -s ${THIS_DIR}/$file ${HOME}
 done
+
+# macOS向けの処理
+if [ "$(uname)" == 'Darwin' ]; then
+    # Homebrew
+    if ! which brew > /dev/null; then
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+    brew tap Homebrew/bundle
+    brew bundle
+fi
