462b038068b73bfdce1dd39cd38b690b66350b94
FromAtom <fromatom@gmail.com>
2024-11-22 00:56:01 +0900

    シンボリックリンクを上書きするオプションを追加

--- setup.sh
+++ setup.sh
@@ -9,19 +9,19 @@ cd $THIS_DIR
 DOT_FILES=( .zsh .zshrc .emacs.d )
 for file in ${DOT_FILES[@]}
 do
-    ln -s ${THIS_DIR}/$file ${HOME}
+    ln -sf ${THIS_DIR}/$file ${HOME}
 done
 
 # link git/config
 if [ -r ${THIS_DIR}/git/config ]; then
   [ ! -d ~/.config/git ] && mkdir -pv ~/.config/git
-  ln -sn "${THIS_DIR}"/git/config ~/.config/git/config
+  ln -snf "${THIS_DIR}"/git/config ~/.config/git/config
 fi
 
 # link git/ignore
 if [ -r ${THIS_DIR}/git/ignore ]; then
   [ ! -d ~/.config/git ] && mkdir -pv ~/.config/git
-  ln -sn ${THIS_DIR}/git/ignore ~/.config/git/ignore
+  ln -snf ${THIS_DIR}/git/ignore ~/.config/git/ignore
 fi
 
 # macOS向けの処理
